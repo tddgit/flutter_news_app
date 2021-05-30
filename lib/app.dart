d6068d62f26af5b515a4889f59bc6803027050ac
Stas <holidayonmail@gmail.com>
2021-05-30 16:35:45 +0300

    added App

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_news_app/screens/news_list.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'News!',
+      home: NewsList(),
+    );
   }
 }
---
+++ lib/screens/news_list.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NewsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Top News'),
+      ),
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
